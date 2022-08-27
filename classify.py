0d8331293b89b520d1fcb14155aa44b44bd6a702
kkm2025 <112076809+kkm2025@users.noreply.github.com>
2022-08-27 19:59:11 +0530

    Update classify.py

--- classify.py
+++ classify.py
@@ -75,7 +75,6 @@ with tab2:
             make_prediction = model.predict(data)
             label = Labels[np.argmax(make_prediction)]
             st.success("The image contains "+ str.lower(label) + " with probability " + str(np.max(make_prediction)*100)[:5]+"%")
-            st.balloons()
             st.snow()
     
     st.caption("Created by Kirankumar Manda")   
