b3b6fdd129c4ba0d3a9f42bc8be2de25f8ce0a25
kkm2025 <112076809+kkm2025@users.noreply.github.com>
2022-08-27 20:11:31 +0530

    Update classify.py

--- classify.py
+++ classify.py
@@ -78,7 +78,7 @@ with tab2:
     
     st.caption("Created by Kirankumar Manda")   
 with tab3:
-    st.write("Kirankumar Manda")
+    st.write("Know more about Mobilenet V2 [link](%s)"%"https://tfhub.dev/google/imagenet/mobilenet_v2_050_224/feature_vector/5")
     url = "https://share.streamlit.io/mesmith027/streamlit_webapps/main/MC_pi/streamlit_app.py"
     st.write("[Github](%s)" % url)
     st.markdown("check out this [link](%s)" % url)
