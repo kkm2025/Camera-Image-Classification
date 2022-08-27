b8584981f3e3c38e36d0337ec3378a7f78d74020
kkm2025 <112076809+kkm2025@users.noreply.github.com>
2022-08-27 21:10:09 +0530

    Update classify.py

--- classify.py
+++ classify.py
@@ -26,14 +26,9 @@ import urllib.request
 import numpy as np
 import time
 st.set_page_config(
-     page_title="Ex-stream-ly Cool App",
-     page_icon="🧊",
+     page_title="Camera Classification",
+     page_icon="📷",
      layout="wide",
-     initial_sidebar_state="expanded",
-     menu_items={
-         'Get Help': 'https://www.extremelycoolapp.com/help',
-         'Report a bug': "https://www.extremelycoolapp.com/bug",
-         'About': "# This is a header. This is an *extremely* cool app!"
      }
  )
 st.markdown("""# Image Classification""")
